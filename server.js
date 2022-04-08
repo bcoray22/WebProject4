e7883d8702087c8d495b2812eeb43b6c3443a47e
bcoray22 <59848205+bcoray22@users.noreply.github.com>
2022-04-08 13:01:09 -0400

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const expressApp = require('./app');
-const dotenv = require('dotenv');
-
-dotenv.config(); // reads file
-
-// listen to client requests in socket
-const server = expressApp.app.listen(process.env.Port,process.env.Host, function(){
-    console.log(`Server running in ${process.env.Host}:${process.env.Port}`);
-    });  
